b247dfb16a4efb0027e512c7123f52d2f92a3870
shiva gupta <shivagupta5607@gmail.com>
2025-12-29 21:26:55 +0530

    component structure setup

---
+++ README.md
@@ -0,0 +1,10 @@
+# DevTinder
+
+- Create a vite + react application.
+- install Tailwind CSS
+- install Daisy UI
+- Create a Navbar.jsx component file.
+- install react-router
+- Create BrowserRouter > Routes > Route=/ Body > RouteChildren
+- Create Outlet in Body component > Navbar , {Outlet}
+- Create Footer in Body component > Navbar , {Outlet} , Footer
---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import Body from "./Body";
+import Login from "./Login";
+import Profile from "./Profile";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter basename='/'>
+        <Routes>
+          <Route
+            path='/'
+            element={<Body />}>
+            <Route
+              path='/login'
+              element={<Login />}
+            />
+            <Route
+              path='/profile'
+              element={<Profile />}
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
